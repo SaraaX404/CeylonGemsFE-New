95d7c4a818548dfbef45f2a0514319ffb1a5fe8c
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-08-31 18:36:33 +0530

    feat:Add how it works screen

---
+++ src/pages/how-it-works/index.tsx
@@ -0,0 +1,85 @@
+import { Layout } from "@/components"
+
+export default ()=>{
+    return(
+    <Layout>
+  <div className="bg-gray-700 h-[400px] grid grid-cols-2">
+                <div className="flex flex-col items-start mt-[12%] ml-[12%]">
+                <h1 className="text-white text-[42px] font-bold">How It Works</h1>
+                <h1 className="text-white text-[22px] font-semibold">Home / How It Works</h1>
+                </div>
+                </div>
+        <div className="grid grid-cols-2 gap-6 mt-[5%] mx-[10%]">
+            <div className="flex flex-col items-start justify-start">
+                <h1 className="text-green-500 text-[38px] font-bold">01.</h1>
+                <h1 className="text-gray-800 text-[28px] font-bold">Register Now & Start Selleing Your Things</h1>
+                <div>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Step into a world where exquisite gems find their perfect admirers – your journey of buying and selling precious stones starts here. With a simple registration, you're poised to explore a dazzling array of gemstones, from radiant diamonds to vibrant colored gems.
+    </h1>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">Experience confident real-time bidding. Start your gem-filled journey today – register now and join a community that cherishes the extraordinary.</h1>
+   
+</div>
+
+            </div>  
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-[5%] mx-[10%]">
+            <div></div>
+            <div className="flex flex-col items-start justify-start">
+                <h1 className="text-green-500 text-[38px] font-bold">02.</h1>
+                <h1 className="text-gray-800 text-[28px] font-bold">Add Your Stones for bidding</h1>
+                <div>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Elevate your collection with us! Uncover splendid gemstones on our exclusive platform. From radiant diamonds to captivating colored gems, explore an exquisite range with a simple registration.
+    </h1>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Immerse in real-time bidding's excitement – each moment promises rare gems. Start your journey now, connect with a community sharing your enthusiasm for these creations. Your next gem awaits, ready to be cherished among fellow connoisseurs.
+    </h1>
+</div>
+
+
+            </div>  
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-[5%] mx-[10%]">
+            
+            <div className="flex flex-col items-start justify-start">
+                <h1 className="text-green-500 text-[38px] font-bold">03.</h1>
+                <h1 className="text-gray-800 text-[28px] font-bold">Select a Stone</h1>
+                <div>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Explore the art of selecting exquisite gemstones with us! Embark on a journey where each stone holds beauty and value. Register now to discover a diverse selection of gems, from timeless diamonds to captivating colored stones.
+    </h1>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Immerse yourself in the thrill of choice – each gem tells a unique story. Join our platform and become part of a community passionate about these treasures. Your perfect gemstone awaits, ready to be selected and cherished for a lifetime.
+    </h1>
+</div>
+
+
+            </div>  
+        </div>
+
+        <div className="grid grid-cols-2 gap-6 mt-[5%] mx-[10%]">
+            <div></div>
+            <div className="flex flex-col items-start justify-start">
+                <h1 className="text-green-500 text-[38px] font-bold">04.</h1>
+                <h1 className="text-gray-800 text-[28px] font-bold">Bid and Win</h1>
+                <div>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Dive into a realm of excitement – bid and win stunning gems! Fuel your passion for precious stones and engage in the art of bidding. With an easy registration, access a curated collection, featuring everything from dazzling diamonds to captivating colored treasures.
+    </h1>
+    <h1 className="text-gray-600 text-[18px] font-semibold leading-9 text-justify">
+        Feel the rush of real-time bidding, where each click could lead to claiming a remarkable gem. Join us today and become part of a community that shares your appreciation for these extraordinary natural wonders. Your winning bid awaits – embark on your journey of bidding and acquiring timeless beauty now.
+    </h1>
+</div>
+
+            </div>  
+        </div>
+        <div className="flex flex-col items-center justify-center mx-[30%] mt-[7%]">
+            <h1 className="text-gray-800 text-[38px] font-bold" >Why choose Us?</h1>
+            <h1 className="text-gray-600 text-[18px] font-semibold mt-[3%] text-center">Experience the best on the world's largest Bidding marketplace with our exceptional products. Your smile, success, and future growth drive us to be your perfect bidding partner.</h1>
+        </div>
+
+
+    </Layout>
+    )
+}
