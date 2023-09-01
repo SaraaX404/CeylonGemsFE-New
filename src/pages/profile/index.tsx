a551cb71777a3ee48caa4a9c75da98249734b0fb
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-09-02 02:45:30 +0530

    feat:Add dashboard tab

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,9 @@
 import { Layout } from "@/components";
 import { AiOutlineUser, AiFillShopping } from "react-icons/ai";
-import { BiSolidDashboard, BiLogOutCircle } from "react-icons/bi";
+import { BiSolidDashboard, BiLogOutCircle, BiLineChart } from "react-icons/bi";
 import { BsGem } from "react-icons/bs";
 import { useState } from "react";
+import { FaCheck, FaClock, FaShoppingBag } from "react-icons/fa";
 
 export default () => {
   const [state, setState] = useState<
@@ -20,7 +21,7 @@ export default () => {
         </div>
       </div>
       <div className="flex flex-row justify-start mx-[10%] mt-[5%]">
-        <div className="flex flex-col w-[25%]  h-[500px] m-5">
+        <div className="flex flex-col w-[25%]  h-[500px] m-3">
           {state != "Profile" && (
             <div onClick={()=> setState("Profile")} className="group bg-white m-2 hover:bg-green-600 transition-colors duration-300 border-neutral-50 rounded cursor-pointer flex flex-row items-center justify-between">
               <h1 className="px-4 py-2 text-gray-600 group-hover:text-white font-semibold text-[22px] transition-colors duration-600">
@@ -108,72 +109,121 @@ export default () => {
             />
           </div>
         </div>
-        <div className="flex flex-col items-start ml-[7%] bg-white w-[80%]">
-            <div className="flex flex-row items-center justify-start gap-5 m-10">
-                <img src="https://th.bing.com/th/id/OIP.13uCmzOQ0gNtEeMTeIqUsAHaHa?pid=ImgDet&rs=1" width={100} className="rounded-full"/>
-                <div className="flex flex-col justify-center">
-                    <h1 className="text-gray-800 text-[30px] font-semibold">
-                    Sara Tancredi
-                    </h1>
-                    <div className="flex flex-row items-center">
-                    <h1 className="text-gray-500 text-[22px] font-semibold">
-                    Sri Lanka
-                    </h1>
-                    <img src="https://th.bing.com/th/id/OIP.bcp-2ZvzcfHtNmVGTY0__wHaHa?pid=ImgDet&rs=1" width={22} className="rounded-full ml-3"/>
-                    </div>
-                    
-                    
+        {state == "Profile"&&(
+ <div className="flex flex-col items-start m-3 bg-white w-[63%]">
+ <div className="flex flex-row items-center justify-start gap-5 m-10">
+     <img src="https://th.bing.com/th/id/OIP.13uCmzOQ0gNtEeMTeIqUsAHaHa?pid=ImgDet&rs=1" width={100} className="rounded-full"/>
+     <div className="flex flex-col justify-center">
+         <h1 className="text-gray-800 text-[30px] font-semibold">
+         Sara Tancredi
+         </h1>
+         <div className="flex flex-row items-center">
+         <h1 className="text-gray-500 text-[22px] font-semibold">
+         Sri Lanka
+         </h1>
+         <img src="https://th.bing.com/th/id/OIP.bcp-2ZvzcfHtNmVGTY0__wHaHa?pid=ImgDet&rs=1" width={22} className="rounded-full ml-3"/>
+         </div>
+         
+         
 
-                    
+         
+     </div>
+    
+ </div>
+ <div className="grid grid-cols-2 w-[90%] mx-[5%] gap-5 mb-[5%]">
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">First Name*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your First Name" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">Last Name*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your Last Name" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">Contact Number*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="0784455785" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">Email*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your Email" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+    
+     <div className="flex flex-col gap-1 col-span-2">
+         <h1 className="text-gray-800 text-[16px] font-normal ">Address Line 1*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="No 01, Apartment 06" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1 col-span-2">
+         <h1 className="text-gray-800 text-[16px] font-normal">Address Line 2</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="albuquerque" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">Country*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="USA" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">State*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="New York" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">City*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="New Mexico" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+    
+     <div className="flex flex-col gap-1">
+         <h1 className="text-gray-800 text-[16px] font-normal">Zip Code*</h1>
+         <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="70200" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
+     </div>
+    
+ </div>
+</div>
+        )}
+
+        {state== "Dashboard"&&(
+                <div className="  w-[63%] m-3">
+                    <div className="grid grid-cols-2">
+                    <div className="flex flex-col mx-5 my-2 rounded-sm border shadow h-[88%] hover:scale-105 transition-transform cursor-pointer bg-white">
+                                <div className="flex flex-row items-center bg-gray-800 w-[100%]">
+                                    <h1 className="text-white text-[16px] font-bold mx-3 my-2">Bid Pending</h1>
+                                </div>
+                                <div className="flex flex-row justify-between items-center m-3">
+                                    <h1 className="text-gray-600 text-[48px]">20</h1>
+                                    <FaClock className="text-gray-600 text-[48px]"/>
+                                </div>
+                    </div>
+                    <div className="flex flex-col mx-5 my-2 border shadow h-[88%] hover:scale-105 transition-transform cursor-pointer bg-white">
+                                <div className="flex flex-row items-center bg-gray-800 w-[100%]">
+                                    <h1 className="text-white text-[16px] font-bold mx-3 my-2">Bid Accepted</h1>
+                                </div>
+                                <div className="flex flex-row justify-between items-center m-3">
+                                    <h1 className="text-gray-600 text-[48px]">40</h1>
+                                    <FaCheck className="text-gray-600 text-[48px]"/>
+                                </div>
+                    </div>
+                    <div className="flex flex-col mx-5 my-2 border shadow h-[88%] hover:scale-105 transition-transform cursor-pointer bg-white">
+                                <div className="flex flex-row items-center bg-gray-800 w-[100%]">
+                                    <h1 className="text-white text-[16px] font-bold mx-3 my-2">Total Listenings</h1>
+                                </div>
+                                <div className="flex flex-row justify-between items-center m-3">
+                                    <h1 className="text-gray-600 text-[48px]">40</h1>
+                                    <FaShoppingBag className="text-gray-600 text-[48px]"/>
+                                </div>
+                    </div>
+                    <div className="flex flex-col mx-5 my-2 border shadow h-[88%] hover:scale-105 transition-transform cursor-pointer bg-white">
+                                <div className="flex flex-row items-center bg-gray-800 w-[100%]">
+                                    <h1 className="text-white text-[16px] font-bold mx-3 my-2">Total Sales</h1>
+                                </div>
+                                <div className="flex flex-row justify-between items-center m-3">
+                                    <h1 className="text-gray-600 text-[48px]">80</h1>
+                                    <BiLineChart className="text-gray-600 text-[48px]"/>
+                                </div>
+                    </div>
+                    </div>
+                
+                
                 </div>
-               
-            </div>
-            <div className="grid grid-cols-2 w-[90%] mx-[5%] gap-5 mb-[5%]">
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">First Name*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your First Name" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Last Name*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your Last Name" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Contact Number*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="0784455785" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Email*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="Your Email" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-               
-                <div className="flex flex-col gap-1 col-span-2">
-                    <h1 className="text-gray-800 text-[16px] font-normal ">Address Line 1*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="No 01, Apartment 06" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1 col-span-2">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Address Line 2</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="albuquerque" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Country*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="USA" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">State*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="New York" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">City*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="New Mexico" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-               
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-gray-800 text-[16px] font-normal">Zip Code*</h1>
-                    <div className="flex flex-col rounded shadow border border-gray-200"><input placeholder="70200" className="w-[100%] px-2 py-3  focus:outline-green-500 focus:ring-1 focus:ring-green-500"/></div>
-                </div>
-               
-            </div>
-        </div>
+        )
+        }
+       
       </div>
     </Layout>
   );
