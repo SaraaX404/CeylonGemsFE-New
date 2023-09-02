f3a38b6c423b9b63b78518e0d8040a1e337e6521
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-09-03 02:55:38 +0530

    patch: Update my stones tab

--- src/components/AuctionCard/index.tsx
+++ src/components/AuctionCard/index.tsx
@@ -24,7 +24,8 @@ export default (props:PropTypes) => {
         <div className="m-3">
             <h1 className="text-gray-700 text-[24px] font-bold">Golden Ruby {props.isProfile?"":"for Sale"}</h1>
             <div className="flex flex-row items-center mt-[3%]">
-                <h1 className="text-gray-500 text-[16px] font-normal">Bidding Price:</h1><h1 className="text-gray-700 text-[16px] font-semibold">$85.9</h1>
+              {props.isProfile? <><h1 className="text-gray-500 text-[16px] font-normal">Total Bids:</h1><h1 className="text-gray-700 text-[16px] font-semibold">20</h1></>: <><h1 className="text-gray-500 text-[16px] font-normal">Bidding Price:</h1><h1 className="text-gray-700 text-[16px] font-semibold">$85.9</h1></>}
+               
             </div>
             <div className="flex flex-row justify-between items-center mt-[3%]">
                 <div className="bg-green-600 border-neutral-50 rounded"><h1 className="text-gray-100 text-[16px] font-semibold px-3 py-2 "> {props.isProfile?"Close":"Place A"}  Bid</h1></div>
