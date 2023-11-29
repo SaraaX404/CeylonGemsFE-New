5204a5a42f729679477b81281e7db5318abd4cc4
Thathsara <thathsara@wireapps.co.uk>
2023-11-29 17:28:48 +0530

    complete chat

--- src/components/ChatBody/chat.component.tsx
+++ src/components/ChatBody/chat.component.tsx
@@ -18,101 +18,126 @@ const botIco =
   "https://th.bing.com/th/id/OIP.C5v0eJ_tW4UiG9zYK6OWcAHaHa?pid=ImgDet&rs=1";
 
 interface ChatMessage {
-  value:{
-    message: string;
-    receiver: string;
-    sender: string;
-  }
-  
+  message: string;
+  receiver: string;
+  sender: string;
 }
+type User = {
+  name: string;
+  id: string;
+};
 
 type Props = {
   name?: string;
   id?: string;
+  setReceiverID: (x: User) => void;
+  receiverID: User;
 };
 
-const App = ({ name, id }: Props) => {
+const App = ({ name, id, setReceiverID, receiverID }: Props) => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
   const [typing, setTyping] = useState(false);
   const UserCTX = useUserContext();
 
+  useEffect(() => {
+    if (receiverID?.id == "") {
+      setReceiverID(
+        {
+          id: id || "",
+          name: name || "",
+        } || ({} as User)
+      );
+    }
+  }, [id]);
+
   const handleSendMessage = (text: string) => {
-    const databaseRef = firebase
-      .database()
-      .ref(`/chat/${UserCTX.user?._id}/${id}`);
+    const databaseRef = firebase.database().ref(`/chat/${UserCTX.user?._id}`);
     databaseRef.push({
       value: {
-        sender: UserCTX.user?._id || '',
+        sender: UserCTX.user?._id || "",
         message: text,
-        receiver: id,
+        receiver: receiverID,
+      },
+    });
+
+    const databaseRef2 = firebase.database().ref(`/chat/${receiverID}`);
+    databaseRef2.push({
+      value: {
+        sender: UserCTX.user?._id || "",
+        message: text,
+        receiver: receiverID,
       },
     });
   };
 
   useEffect(() => {
     // Fetch data from Firebase
-    const databaseRef = firebase
-      .database()
-      .ref(`/chat/${UserCTX.user?._id}/${id}`); // Replace with your database path
-    databaseRef.on("value", (snapshot) => {
+    console.log(receiverID);
+    const databaseRef = firebase.database().ref(`/chat/${UserCTX.user?._id}`);
+
+    const handleSnapshot = (snapshot: { val: () => any }) => {
       const dataSnapshot = snapshot.val();
       const dataArray: ChatMessage[] = dataSnapshot
-        ? Object.values(dataSnapshot)
+        ? Object.values(dataSnapshot).map((x: any) => x.value)
         : [];
-      setMessages(dataArray)
-    });
+      console.log(dataArray, "de");
 
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      databaseRef.off("value");
+      setMessages(dataArray);
     };
-  }, []);
-  
+
+    // Attach the event listener
+    databaseRef.on("value", handleSnapshot);
+
+    // Cleanup the event listener when the component unmounts or when receiverID changes
+    return () => {
+      databaseRef.off("value", handleSnapshot);
+    };
+  }, [receiverID, id]); // Include receiverID in the dependency array
 
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-      <ChatContainer>
-        <ConversationHeader>
-          <Avatar
-            src="https://th.bing.com/th/id/R.f8f17e935d16c6627dd37f5c784c3c9e?rik=%2fTOJbw4sTquGLg&pid=ImgRaw&r=0"
-            name="Kasun Kavinda"
+      {receiverID.name && (
+        <ChatContainer>
+          <ConversationHeader>
+            <Avatar
+              src="https://th.bing.com/th/id/R.f8f17e935d16c6627dd37f5c784c3c9e?rik=%2fTOJbw4sTquGLg&pid=ImgRaw&r=0"
+              name="Kasun Kavinda"
+            />
+            <ConversationHeader.Content
+              userName={receiverID?.name || ""}
+              info="Active"
+            />
+            <ConversationHeader.Actions>
+              <InfoButton />
+            </ConversationHeader.Actions>
+          </ConversationHeader>
+          <MessageList
+            typingIndicator={
+              typing ? <TypingIndicator content={`${name} is typing`} /> : null
+            }
+          >
+            {messages?.map((message, index) => {
+              return (
+                <Message
+                  key={index}
+                  model={{
+                    message: message?.message,
+                    direction:
+                      message?.sender == UserCTX?.user?._id
+                        ? "outgoing"
+                        : "incoming",
+                    position: "single",
+                  }}
+                ></Message>
+              );
+            })}
+          </MessageList>
+          <MessageInput
+            placeholder="Type a message..."
+            onSend={handleSendMessage}
           />
-          <ConversationHeader.Content
-            userName={name ? name : "Kasun Kavinda"}
-            info="Active"
-          />
-          <ConversationHeader.Actions>
-            <InfoButton />
-          </ConversationHeader.Actions>
-        </ConversationHeader>
-        <MessageList
-          typingIndicator={
-            typing ? <TypingIndicator content={`${name} is typing`} /> : null
-          }
-        >
-          {messages.map((message, index) => {
-            console.log(message)
-
-            return (
-              <Message
-                key={index}
-                model={{
-                  message: message.value.message,
-                  direction:
-                    message.value.sender == UserCTX.user?._id
-                      ? "outgoing"
-                      : "incoming",
-                  position: "single",
-                }}
-              ></Message>
-            );
-          })}
-        </MessageList>
-        <MessageInput
-          placeholder="Type a message..."
-          onSend={handleSendMessage}
-        />
-      </ChatContainer>
+        </ChatContainer>
+      )}
     </div>
   );
 };
--- src/components/ChatBody/index.tsx
+++ src/components/ChatBody/index.tsx
@@ -19,6 +19,7 @@ export default ({ name, id }: PropTypes) => {
 
   const UserCTX = useUserContext();
   const [receiverName, setReceiverName] = useState<User[]>([]);
+  const [receiverID, setReceiverID] = useState<User>({} as User)
 
   interface ChatMessage {
     value: {
@@ -27,28 +28,56 @@ export default ({ name, id }: PropTypes) => {
       sender: string;
     };
   }
+
+  const fetchData = async() => { 
+    const response = await fetch(
+      `http://localhost:3000/users/${id}`
+    );
+
+
+    const data = await response?.json();
+
+    setReceiverName([...receiverName, {id:id || '', name:data.name}])
+    
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[id])
   useEffect(() => {
     // Fetch data from Firebase
     const databaseRef = firebase.database().ref(`/chat/${UserCTX.user?._id}`); // Replace with your database path
     databaseRef.on("value", (snapshot) => {
+      console.log(`/chat/${UserCTX.user?._id}`)
       const dataSnapshot = snapshot.val();
+      console.log('de', dataSnapshot)
       const dataArray: ChatMessage[] = dataSnapshot
         ? Object.values(dataSnapshot)
         : [];
-      dataArray.map((x: any) => {
-        Object.keys(x).map(async (y: any) => {
+      console.log(dataArray, 'da')
+      dataArray.map(async(x: any) => {
+       
+          let u:string | null = null
+          if(x.value.receiver == UserCTX.user?._id){
+            u = x.value.sender
+          }else{
+            u = x.value.receiver
+          }
           // Fetch the user name by ID
           const response = await fetch(
-            `http://localhost:3000/users/${x[y].value.receiver}`
+            `http://localhost:3000/users/${u}`
           );
-          const data = await response.json();
+
+          const data = await response?.json();
           const user: User = {
-            id: x[y].value.receiver,
+            id: u || '',
             name: data.name,
-          }; // Adjust this based on your actual API response
+          };
+
+          console.log(user, 'us')
 
           setReceiverName([...receiverName, user]);
-        });
+      
       });
     });
 
@@ -56,23 +85,28 @@ export default ({ name, id }: PropTypes) => {
     return () => {
       databaseRef.off("value");
     };
-  }, []);
+  }, [UserCTX.user?._id]);
+
+  
+
+
 
 
   return (
     <div className="flex flex-row">
       <div className="flex flex-col items-start w-[40%]">
         <Input placeholder="search" className="mx-3 my-3 px-3" />
-        {name && <Item name={name} />}
         {receiverName.map((x)=>{
             return(
-                (name || '') != x.name && <Item name={x.name} />
+                (name || '') != x.name && <div onClick={()=>{
+                 
+                  setReceiverID({id:x.id, name:x.name})
+                }}><Item name={x.name} /></div>
             )
-            
-})}
+    })}
       </div>
       <div className="w-[65%]  mx-[3%]">
-        <ChatBody name={name} id={id} />
+        <ChatBody name={name} id={id}  receiverID={receiverID} setReceiverID={setReceiverID} />
       </div>
     </div>
   );
